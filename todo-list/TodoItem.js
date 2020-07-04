de1c9c2c0a6803f254dd5846e7fb514e53042a7c
Gangadi Sai Prakash Reddy <51281034+GSPR93@users.noreply.github.com>
2020-07-04 22:46:04 +0530

    Create TodoItem.js

---
+++ todo-list/TodoItem.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function TodoItem(props) {
+    const style =  props.item.completed?{textDecoration:"line-through",color:"orange"}:{}
+    return (
+        <div className="todo-item">
+            <input 
+                type="checkbox" 
+                checked={props.item.completed} 
+                onChange={() => props.handleChange(props.item.id)}
+            />
+            <p style={style}>{props.item.text}</p>
+        </div>
+    )
+}
+
+export default TodoItem
