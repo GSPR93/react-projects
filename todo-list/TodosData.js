301c0c0abefff9cec8010592fc9b8bc6c0d86eca
Gangadi Sai Prakash Reddy <51281034+GSPR93@users.noreply.github.com>
2020-07-04 22:47:40 +0530

    Create TodosData.js

---
+++ todo-list/TodosData.js
@@ -0,0 +1,30 @@
+ (28 sloc) 442 Bytes
+const todosData = [
+    {
+        id: 1,
+        text: "Learn react basics",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Workout",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Movie",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Exam",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Call mom",
+        completed: false
+    }
+]
+
+export default todosData
